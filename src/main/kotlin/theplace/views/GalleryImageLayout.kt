9a211ef109f9ca58c95ab72d0a23b9b0025006c8
m <mmailm@mail.ru>
2016-04-04 13:02:29 +0800

    fixes

--- src/main/kotlin/theplace/views/GalleryImageLayout.kt
+++ src/main/kotlin/theplace/views/GalleryImageLayout.kt
@@ -2,6 +2,7 @@ package theplace.views
 
 import javafx.beans.value.ChangeListener
 import javafx.event.EventHandler
+import javafx.event.EventType
 import javafx.scene.Parent
 import javafx.scene.control.Button
 import javafx.scene.control.CheckBox
@@ -9,8 +10,12 @@ import javafx.scene.image.Image
 import javafx.scene.image.ImageView
 import javafx.scene.layout.AnchorPane
 import javafx.scene.layout.Border
+import javafx.scene.layout.BorderPane
+import javafx.scene.layout.Pane
 import theplace.parsers.elements.GalleryImage
 import tornadofx.Fragment
+import tornadofx.add
+import javafx.scene.input.MouseEvent
 import java.io.InputStream
 import java.nio.file.Files
 import java.nio.file.Paths
@@ -21,31 +26,37 @@ import java.nio.file.Paths
 class GalleryImageLayout(val img: GalleryImage) : Fragment() {
     override val root: AnchorPane by fxml()
     val image: ImageView by fxid()
-    val btnDownload: Button by fxid()
+    //    val btnDownload: Button by fxid()
+    val imageContainer: AnchorPane by fxid()
+    val overlayPane: BorderPane by fxid()
+
     var img_data: InputStream? = null
     var iconDownload: ImageView = ImageView(resources["../../images/download.png"])
     var iconRemove: ImageView = ImageView(resources["../../images/Trash.png"])
     var iconLoading: ImageView = ImageView(resources["../../images/loading.gif"])
+    var isLoading = false
 
     var dir_path: String = "./downloads/"
 
     fun update_interface() {
         var exists = img.exists(dir_path)
-        btnDownload.text = if (exists) "" else ""
-        btnDownload.graphic = if (exists) iconRemove else iconDownload
-        btnDownload.isDisable = false
+        //        btnDownload.text = if (exists) "" else ""
+        //        btnDownload.graphic = if (exists) iconRemove else iconDownload
+        //        btnDownload.isDisable = false
 
         if (exists) {
             root.styleClass.add("exists")
-            btnDownload.styleClass.add("exists")
+            //            btnDownload.styleClass.add("exists")
         } else {
             root.styleClass.remove("exists")
-            btnDownload.styleClass.remove("exists")
+            //            btnDownload.styleClass.remove("exists")
         }
     }
 
     init {
         update_interface()
+
+        overlayPane.center = iconLoading
 
         background {
             img_data = img.download_thumb()
@@ -53,15 +64,28 @@ class GalleryImageLayout(val img: GalleryImage) : Fragment() {
             image.image = Image(img_data)
         }
 
+        root.addEventHandler(MouseEvent.MOUSE_ENTERED_TARGET, EventHandler {
+            if (isLoading) {
+                return@EventHandler
+            }
+            overlayPane.isVisible = true
+            var exists = img.exists(dir_path)
+            overlayPane.center = if (exists) iconRemove else iconDownload
+        })
+
+        root.addEventHandler(MouseEvent.MOUSE_EXITED_TARGET, EventHandler {
+            overlayPane.isVisible = isLoading || false
+        })
+
         listOf(iconRemove, iconDownload, iconLoading).forEach {
-            it.fitHeight = 20.0
             it.isPreserveRatio = true
             it.isSmooth = true
         }
 
-        btnDownload.onAction = EventHandler {
-            btnDownload.graphic = iconLoading
-            btnDownload.isDisable = true
+        root.onMouseClicked = EventHandler {
+            overlayPane.center = iconLoading
+            overlayPane.isVisible = true
+            isLoading = true
             background {
                 if (img.exists(dir_path)) {
                     Files.delete(Paths.get(img.get_path(dir_path)))
@@ -69,6 +93,8 @@ class GalleryImageLayout(val img: GalleryImage) : Fragment() {
                     img.save_to_file(dir_path)
                 }
             } ui {
+                overlayPane.isVisible = false
+                isLoading = false
                 update_interface()
             }
         }
